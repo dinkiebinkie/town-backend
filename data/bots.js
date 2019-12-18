b8e29c26d6c8b45134016d3e94567cb3d685e148
dinkiebinkie <b@binkie.town>
2019-12-18 15:40:22 -0500

    baby working

--- data/bots.js
+++ data/bots.js
@@ -1,25 +1,33 @@
 const config = require("../config");
 module.exports = [
-  // {
-  //   bot_id: "futuristic_baby_names",
-  //   bot_name: "Futuristic Baby Names",
-  //   tweet_beginnings: [],
-  //   bot_type: "twitter",
-  //   bot_url: "https://twitter.com/BabyFuturistic/", // where does the bot live
-  //   number_of_likes_per_tweet: 20, // how many tweets does the bot like when it tweets
-  //   tweet_storage_length: 1000, // how many tweets are requested if it's running low
-  //   timer: 10, // how long (ms) until next tweet? variation of 75-125%
-  //   hashtags: ["binkie", "baby"], // any hashtags you think the bot should include if the tweet is short enough or for favoriting
-  //   hashtagPercentage: 75, // % of the time you think the bot should send a tweet
-  //   authentication: {
-  //     consumer_key: process.env.TWITTER_API_KEY_FUTURISTIC_BABY_NAMES,
-  //     consumer_secret: process.env.TWITTER_API_SECRET_KEY_FUTURISTIC_BABY_NAMES,
-  //     access_token_key: process.env.TWITTER_ACCESS_TOKEN_FUTURISTIC_BABY_NAMES,
-  //     access_token_secret:
-  //       process.env.TWITTER_ACCESS_TOKEN_SECRET_FUTURISTIC_BABY_NAMES
-  //   },
-  //   videoUrl: "" // video related to this bot on my YT channel xSZ
-  // },
+  {
+    bot_id: "futuristic_baby_names",
+    bot_name: "Futuristic Baby Names",
+    tweet_beginnings: [],
+    bot_type: "twitter",
+    bot_url: "https://twitter.com/BabyFuturistic/", // where does the bot live
+    number_of_likes_per_tweet: 15, // how many tweets does the bot like when it tweets
+    tweet_storage_length: 1000, // how many tweets are requested if it's running low
+    timer: config.testTweeting ? 5000 : 10 * 60 * 60 * 1000, // how long (ms) until next tweet? variation of 50-150%
+    hashtags: [
+      "baby",
+      "babyname",
+      "babynames",
+      "HORRIDBABYNAMES",
+      "NamesForMyChildren",
+      "names",
+      "fail"
+    ], // any hashtags you think the bot should include if the tweet is short enough or for favoriting
+    hashtagPercentage: 75, // % of the time you think the bot should send a tweet
+    authentication: {
+      consumer_key: process.env.TWITTER_API_KEY_FUTURISTIC_BABY_NAMES,
+      consumer_secret: process.env.TWITTER_API_SECRET_KEY_FUTURISTIC_BABY_NAMES,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_FUTURISTIC_BABY_NAMES,
+      access_token_secret:
+        process.env.TWITTER_ACCESS_TOKEN_SECRET_FUTURISTIC_BABY_NAMES
+    },
+    videoUrl: "" // video related to this bot on my YT channel xSZ
+  },
   {
     bot_id: "inspirational_quotes",
     bot_name: "Inspirational Quotes",
--- utility.js
+++ utility.js
@@ -2,17 +2,6 @@
 const uuidv4 = require("uuid/v4");
 const bots = require("./data/bots");
 // const naughtyWords = require("./data/naughtyWords");
-const fs = require("fs");
-
-// set timeout for next action/tweet
-function setTimer(id) {
-  const thisBot = bots.find(bot => bot.id === id);
-  const botTimer = thisBot.timer;
-
-  // convert timer to MS
-  // random number between 70% - 130% of config timer set from bots.js
-  const timer = (Math.random() * (1.3 - 0.7) + 0.7) * 60 * 60 * 1000 * botTimer;
-}
 
 // tweet filter
 // filter words from tweets - if it is a bad word don't tweet
@@ -35,29 +24,29 @@ function generateRandomID() {
   return uuidv4();
 }
 
-async function getTweetsFromFile(bot_id) {
-  return new Promise((res, rej) => {
-    const tweets = [];
-    let lineIndex = 0;
+// async function getTweetsFromFile(bot_id) {
+//   return new Promise((res, rej) => {
+//     const tweets = [];
+//     let lineIndex = 0;
 
-    const lineReader = require("readline").createInterface({
-      input: require("fs").createReadStream(`./data/tweets_txt/${bot_id}.txt`)
-    });
+//     const lineReader = require("readline").createInterface({
+//       input: require("fs").createReadStream(`./data/tweets_txt/${bot_id}.txt`)
+//     });
 
-    lineReader
-      .on("line", line => {
-        console.log("line", line);
+//     lineReader
+//       .on("line", line => {
+//         console.log("line", line);
 
-        tweets.push(line);
-        lineIndex++;
-        if (lineIndex >= 100) {
-          lineReader.close();
-          res(tweets);
-        }
-      })
-      .on("close", line => res(tweets));
-  });
-}
+//         tweets.push(line);
+//         lineIndex++;
+//         if (lineIndex >= 100) {
+//           lineReader.close();
+//           res(tweets);
+//         }
+//       })
+//       .on("close", line => res(tweets));
+//   });
+// }
 
 exports.generateRandomID = generateRandomID;
-exports.getTweetsFromFile = getTweetsFromFile;
+// exports.getTweetsFromFile = getTweetsFromFile;
