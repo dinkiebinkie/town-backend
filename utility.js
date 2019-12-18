63b5956d5955bceabcc082087e75d0017fe1636f
dinkiebinkie <b@binkie.town>
2019-12-18 12:47:44 -0500

    who cares about naughty words anyways

--- utility.js
+++ utility.js
@@ -1,7 +1,7 @@
 // helper functions ...?
 const uuidv4 = require("uuid/v4");
 const bots = require("./data/bots");
-const naughtyWords = require("./data/naughtyWords");
+// const naughtyWords = require("./data/naughtyWords");
 const fs = require("fs");
 
 // set timeout for next action/tweet
@@ -16,20 +16,20 @@ function setTimer(id) {
 
 // tweet filter
 // filter words from tweets - if it is a bad word don't tweet
-function isTweetNaughty(tweet) {
-  const str = tweet.tweet.toLowerCase();
+// function isTweetNaughty(tweet) {
+//   const str = tweet.tweet.toLowerCase();
 
-  for (let i = 0; i < naughtyWords.length; i++) {
-    const tweetIsNaughty = str.includes(naughtyWords[i]);
+//   for (let i = 0; i < naughtyWords.length; i++) {
+//     const tweetIsNaughty = str.includes(naughtyWords[i]);
 
-    if (tweetIsNaughty) {
-      console.log("tweet is naughty 🙀 ", str, "... Getting new tweet");
-      return true;
-    }
-  }
+//     if (tweetIsNaughty) {
+//       console.log("tweet is naughty 🙀 ", str, "... Getting new tweet");
+//       return true;
+//     }
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 function generateRandomID() {
   return uuidv4();
