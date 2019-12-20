cb7f0754fc739d31c1efc708f4ee2a5768859d47
dinkiebinkie <b@binkie.town>
2019-12-20 17:05:09 -0500

    trying to add heroku no sleep thing

--- tweet/timerStuff.js
+++ tweet/timerStuff.js
@@ -1,11 +1,15 @@
 const bots = require("../data/bots");
 const fetchTweetThenTweet = require("./fetchTweetThenTweet");
 
-const startTimers = () => bots.forEach(bot => restartTimer(bot));
+const startTimers = () => {
+  setInterval(console.log("keeping server alive... i think?"), 1800000);
+  return bots.forEach(bot => restartTimer(bot));
+};
 
 const restartTimer = bot => {
   const rn =
     Math.random() * (bot.timer * 1.5 - bot.timer * 0.5) + bot.timer * 0.5;
+  console.log("setting timer for " + rn + "times for bot" + bot.bot_id);
   return setTimeout(fetchTweetThenTweet, rn, bot.bot_id);
 };
 
