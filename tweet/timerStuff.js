4779231adf6f377f59f94ca6b7e7f082d781ceb2
dinkiebinkie <b@binkie.town>
2019-12-22 16:20:46 -0500

    empty commit to rebuild heroku

--- tweet/timerStuff.js
+++ tweet/timerStuff.js
@@ -12,7 +12,7 @@ const startTimers = () => {
 const restartTimer = bot => {
   const rn =
     Math.random() * (bot.timer * 1.5 - bot.timer * 0.5) + bot.timer * 0.5;
-  console.log("setting timer for " + rn + "times for bot" + bot.bot_id);
+  console.log("setting timer for " + rn + " times for bot " + bot.bot_id);
   return setTimeout(fetchTweetThenTweet, rn, bot.bot_id);
 };
 
